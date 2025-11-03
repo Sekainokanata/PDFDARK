49dcce058ba9741da9122bd990846e36aa065105
SURFACE\satom <h20025@e.hiroshima-cu.ac.jp>
2025-11-03 23:52:00 +0900

    chcange stylesprogram from viewer.html to style.css , add title at toolbar

--- viewer/ui.js
+++ viewer/ui.js
@@ -19,32 +19,37 @@ window.setupShell = function setupShell(origContainer) {
     boxShadow: '0 1px 0 rgba(60,60,60,0.02) inset', color: '#e6e6e6',
   });
 
+  //titleの部分
+  const params = new URLSearchParams(location.search);
+  const file = params.get('file');
+  console.log(file);
+  let pdftitle;
+  if (file) {
+    try { const urlObj = new URL(file, location.href); const filename = urlObj.pathname.split('/').pop() || 'PDF'; pdftitle = decodeURIComponent(filename); }
+    catch (e) { const name = (file.split('/').pop() || 'PDF'); pdftitle = name;}
+  }
+
   // left group
   const leftGroup = document.createElement('div');
   leftGroup.className = 'viewer-toolbar-group';
-  leftGroup.style.display = 'flex'; leftGroup.style.gap = '6px'; leftGroup.style.alignItems = 'center';
+  leftGroup.style.display = 'flex'; leftGroup.style.gap = '6px'; leftGroup.style.alignItems = 'center'; leftGroup.style.position = 'absolute'; leftGroup.style.padding = '0 10px';
+  leftGroup.textContent = `${pdftitle}`;
   //const btnPrev = document.createElement('button'); btnPrev.className = 'viewer-tool-btn'; btnPrev.textContent = '◀';
   
   //const btnNext = document.createElement('button'); btnNext.className = 'viewer-tool-btn'; btnNext.textContent = '▶';
   //leftGroup.appendChild(btnPrev); 
   //leftGroup.appendChild(pageInput); leftGroup.appendChild(pageTotal);
   //leftGroup.appendChild(btnNext);
+  
 
   // center group
   const centerGroup = document.createElement('div');
-  centerGroup.className = 'viewer-toolbar-group'; centerGroup.style.display = 'flex'; centerGroup.style.margin = '0 auto'; centerGroup.style.gap = '6px'; centerGroup.style.alignItems = 'center';
+  centerGroup.className = 'viewer-toolbar-group'; centerGroup.style.display = 'flex'; centerGroup.style.margin = '0 auto'; centerGroup.style.gap = '6px'; centerGroup.style.alignItems = 'center'; centerGroup.style.background = '#3C3C3C'; centerGroup.style.zIndex = '1';
   const pageInput = document.createElement('input'); pageInput.type = 'number'; pageInput.min = 1; pageInput.value = 1; pageInput.style.width = '20px'; pageInput.className = 'viewer-tool-btn aaaaa'; pageInput.style.background = '#1E1E1E'; pageInput.style.textAlign = 'center'; pageInput.style.border = 'none'
   const pageTotal = document.createElement('div'); pageTotal.textContent = `/ ${2}`;
   const btnZoomOut = document.createElement('button'); btnZoomOut.className = 'viewer-tool-btn'; btnZoomOut.textContent = '-';
   //zoomの数字について画面の横幅一定以下で取消
   const zoomVal = document.createElement('input'); zoomVal.id = 'zoom-value'; zoomVal.value = '100%'; zoomVal.style.maxWidth = '10px'; zoomVal.style.background = '#1E1E1E';
-  if (window.matchMedia('(max-width: 767px)').matches) {
-    //スマホ処理
-    zoomVal.style.display = 'none'
-} else if (window.matchMedia('(min-width:768px)').matches) {
-    //PC処理
-    zoomVal.style.display = 'block'
-}
   const btnZoomIn = document.createElement('button'); btnZoomIn.className = 'viewer-tool-btn'; btnZoomIn.textContent = '+';
   const btnFitWidth = document.createElement('button'); btnFitWidth.className = 'viewer-tool-btn'; btnFitWidth.textContent = 'FW'; btnFitWidth.title = 'ページの横幅に合わせる'
   const btnFitPage = document.createElement('button'); btnFitPage.className = 'viewer-tool-btn'; btnFitPage.textContent = 'FP'; btnFitPage.title = 'ページの高さに合わせる'
